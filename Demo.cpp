b1d412c62c650a7877b2035f86056020b9cd4f8c
naor94 <44939883+naor94@users.noreply.github.com>
2019-05-20 06:18:30 -0400

    Update Demo.cpp

--- Demo.cpp
+++ Demo.cpp
@@ -7,6 +7,7 @@
 
 
 #include <iostream>
+#include <sstream>
 using namespace std;
 
 #include "range.hpp"
@@ -16,10 +17,19 @@ using namespace std;
 #include "powerset.hpp"
 using namespace itertools;
 
+template<typename Iterable>
+string iterable_to_string(const Iterable& iterable) {
+	ostringstream ostr;
+	for (auto i: iterable)
+		ostr << i << ",";
+	return ostr.str();
+}
+
 int main() {
 	cout << endl << endl << "Range of ints: " << endl;
 	for (int i: range(5,9))
 		cout << i;    // 5678
+	cout << endl;
 	cout << endl << endl << "Range of doubles: " << endl;
 	for (double i: range(5.1,9.1))
 		cout << i << " ";    // 5.1 6.1 7.1 8.1
@@ -27,8 +37,8 @@ int main() {
 	for (char i: range('a','e'))
 		cout << i << " ";    // a b c d 
 
-	//Note: this example works even without your code.
-	//It shows that a string is also an "iterable" - it can be iterated with a for-each loop.
+	// Note: this example works even without your code.
+	// It shows that a string is also an "iterable" - it can be iterated with a for-each loop.
 	cout << endl << endl << "Standard string: " << endl;
 	for (char i: string("hello"))
 		cout << i << " ";    // prints h e l l o 
@@ -57,6 +67,7 @@ int main() {
 	cout << endl << endl << "Power-set of chain " << endl;
 	for (auto subset: powerset(chain(range('a','c'),range('x','z'))))
 		cout << subset;  // {}{a}{b}{a,b}{x}{a,x}{b,x}{a,b,x}{y}{a,y}{b,y}{a,b,y}{x,y}{a,x,y}{b,x,y}{a,b,x,y}
+	cout << endl;
+	cout << iterable_to_string(powerset(chain(range('a','c'),range('x','z')))) << endl;
 	return 0;
 }
-
