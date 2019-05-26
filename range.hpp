8af51e46353458ff4fb4f50820eab90e49487535
naor94 <44939883+naor94@users.noreply.github.com>
2019-05-26 05:10:19 -0400

    Update range.hpp

--- range.hpp
+++ range.hpp
@@ -1,73 +1,67 @@
-#include <iostream>
 #pragma once
 
-namespace itertools{
+#include <iterator>
+#include<iostream>
 
-template<typename T>
-class range{
- 
+namespace itertools
+{
+template <class T>
+class range
+{
 
-public:
-T start;
-T last;
+    T _start; // __starting point
+    T _end;   // stopping point.
+   
+    class iterator
+    {
 
- 
-range(T b, T e){
-    start=b;
-    last=e;
-}
+    public:
+        T value;
 
-class iterator{
+        //constructor
+        iterator(T v) : value(v){}
 
-private:
-T val;
+        // operators
+        bool operator!=(range::iterator const &other) const
+        { 
+            return value != (other.value);
+        }
 
+    bool operator== (range::iterator const &other) const
+    {
+       return value == other.value;
 
-public:
-iterator(T p = nullptr)
-: val(p) 
-{}
+     }
+        const T operator*() const
+        {
+            return value;
+        }
 
-T operator*() const {
-	return val;
-}
+        iterator &operator++()
+        {
 
-iterator& operator++() {
-    val++;
-	return *this;
-}
+            ++value;
+            return *this;
+        }
 
-const iterator operator++(int) {
- iterator tmp= *this;
- val++;
- return tmp;
-}
-
-
-
-bool operator!=(const iterator& rhs) const {
-	return val != rhs.val;
-}
-
-
-
-}; //end itertor class
+    };
 
 public:
-iterator begin() {
-		return iterator{start};
-	}
-	
-	iterator end() {
-		return iterator{last};
-	}
+
+    range(T from, T to) : _start(from), _end(to) {}  
 
 
 
-};
+
+		iterator begin() const {
+			return iterator(_start);
+		}
+
+		iterator end() const {
+			return iterator(_end);
+		} 
+}; // class
 
 
-}
 
-
-
+} // namespace itertools
