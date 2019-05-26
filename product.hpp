46dc602f24881068b841d72a316072d7d87b26f4
naor94 <44939883+naor94@users.noreply.github.com>
2019-05-26 05:11:24 -0400

    Update product.hpp

--- product.hpp
+++ product.hpp
@@ -1,8 +1,13 @@
-#include "iostream"
+#pragma once
+
+#include <utility>
+#include <assert.h>
+#include <iostream>
 
 //NameSpace for a Tasks
 namespace itertools {
-    
+
+    bool temp = false;
     template <typename T1, typename T2> 
     class product {
     
@@ -11,14 +16,19 @@ namespace itertools {
         T2 iterable_B;
 
     public:
-        product(T1 start, T2 end) :  iterable_A(start), iterable_B(end) {}
+        product(T1 start, T2 end) :  iterable_A(start), iterable_B(end) {
+              temp = false;
+              if(!(end.begin() != end.end())) 
+              temp = true;
+            }
         
-    auto begin(){ 
-        return  iterator<decltype(iterable_A.begin()),decltype(iterable_B.begin())>(iterable_A.begin(), iterable_B.begin()); }  // iteratable object
-
-    auto end() {
-        return iterator<decltype(iterable_A.end()),decltype(iterable_B.end())>(iterable_A.end(), iterable_B.end()); }  // iteratable object  
- 
+        
+   auto begin() const {
+              return iterator<decltype(iterable_A.begin()),decltype(iterable_B.begin())>(iterable_A.begin(), iterable_B.begin());
+             }
+            auto end() const {
+              return iterator<decltype(iterable_A.end()),decltype(iterable_B.end())>(iterable_A.end(), iterable_B.end());
+             }
     template <typename C1, typename C2>
         class iterator {
 
@@ -26,29 +36,36 @@ namespace itertools {
             C1 iter_A; // iterator A
             C2 iter_B; // iterator B
             C2 save_position_B;
+              bool sauv_index;
 
         public:
-            iterator(C1 itA , C2 itB): iter_A(itA) , iter_B(itB) , save_position_B(iter_B) {}
+            iterator(C1 itA , C2 itB): iter_A(itA) , iter_B(itB) , save_position_B(iter_B),sauv_index(false) {}
 
-           iterator<C1,C2>& operator++() {
-               ++iter_B;
-               return *this;
- 
-            }
+         
+              iterator& operator++() {
+              if(!sauv_index)
+              ++iter_B;
+              return *this;
+              }
 
-            std::pair<decltype(*iter_A),decltype(*iter_B)> operator*() const {
+             auto operator*() const
+              {
+               return pair<decltype(*iter_A),decltype(*iter_B)> (*iter_A,*iter_B);
+              }
 
-             return  std::pair<decltype(*iter_A),decltype(*iter_B)> (*iter_A , *iter_B);
-            }
-
-            bool operator!=(iterator<C1,C2>  it){
-                if((iter_A != it.iter_A) && !(iter_B != it.iter_B)){
-                    iter_B = save_position_B;
-                    ++iter_A;
+      
+              bool operator!= (iterator const & other)
+              {
+                if ((iter_A != other.iter_A) && !(iter_B != other.iter_B)){
+                    sauv_index = true;
+                  }
+                if(sauv_index){
+                  iter_B = save_position_B;
+                  ++iter_A;
+                  sauv_index = false;
                 }
-                return (iter_A != it.iter_A);
- 
-            }
+                return (iter_A != other.iter_A && !temp);
+              }
 
 
          
